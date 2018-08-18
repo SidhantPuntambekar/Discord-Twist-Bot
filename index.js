02cdb31eb952bd68cb2f7f562c3bcc71d4499ab6
Saurabh Totey <SaurabhTotey@gmail.com>
2018-08-18 15:51:01 -0600

    Added pg as a dependency

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const Discord = require("discord.js");
+const pg = require("pg");
+
 const client = new Discord.Client();
+let databaseClient;
 
 //If environment variables aren't already available, load them from file
 if (process.env.PORT == undefined) {
     require("dotenv").load()
 } else { //If environment variables are already available, then Heroku is being used
-
+    databaseClient = new pg.Client({ connectionString: process.env.DATABASE_URL });
 }
 
+//Logs the client in
 client.login(`${process.env.DiscordKey}`);
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.id}`);
-});
+//How the bot responds to a message being sent
+client.on('message', message => {
+    console.log(`Received message: '${message.content}'`)
+})
