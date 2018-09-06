8f90121541551a540f029830a9dc5f200fd8f579
Saurabh Totey <SaurabhTotey@gmail.com>
2018-09-06 12:41:06 -0600

    Revert "Switched bot over to TypeScript"
    This reverts commit c7b8d1aea13e7fd0ded336305e2cea2e6e5dcc59.

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Discord = require("discord.js");
-var Dotenv = require("dotenv");
-Dotenv.load();
-var client = new Discord.Client();
-client.login(process.env.DiscordKey);
-client.on("ready", function () {
-    client.channels.array().pop().send("test");
-});
+const Discord = require("discord.js");
+const pg = require("pg");
+
+const client = new Discord.Client();
+let databaseClient;
+
+//If environment variables aren't already available, load them from file
+if (process.env.PORT == undefined) {
+    require("dotenv").load()
+} else { //If environment variables are already available, then Heroku is being used
+    databaseClient = new pg.Client({ connectionString: process.env.DATABASE_URL });
+}
+
+//Logs the client in
+client.login(`${process.env.DiscordKey}`);
+
+//How the bot responds to a message being sent
+client.on('message', message => {
+    //If the command doesn't have the bot mentioned, the bot ignores the message
+    if (!message.cleanContent.includes("@Tornado Twister#8282")) {
+        return;
+    }
+    client.message("Hello");
+})
