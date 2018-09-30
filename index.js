a9766807d1432a3ce92acb0a264c77e0a2ec4214
Saurabh Totey <SaurabhTotey@gmail.com>
2018-09-29 22:40:53 -0600

    Removed broken message replying

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ let databaseClient;
 if (process.env.PORT == undefined) {
     require("dotenv").load()
 } else { //If environment variables are already available, then Heroku is being used
-    databaseClient = new pg.Client({ connectionString: process.env.DATABASE_URL });
+    databaseClient = new pg.Client({connectionString: process.env.DATABASE_URL});
 }
 
 //Logs the client in
 client.login(`${process.env.DiscordKey}`);
 
-//How the bot responds to a message being sent
-client.on('message', message => {
-    //If the command doesn't have the bot mentioned, the bot ignores the message
-    if (!message.cleanContent.includes("@Tornado Twister#8282")) {
-        return;
-    }
-    client.message("Hello");
-})
+client.on('ready', () => {
+
+    //How the bot responds to a message being sent
+    client.on('message', message => {
+        //TODO:
+    });
+
+});
